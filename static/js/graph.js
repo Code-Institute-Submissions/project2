2290990907ab67949dfab28f735ce85adcf4f704
Kashmir Kalirai <kashmirk67@gmail.com>
2017-04-22 14:57:12 +0100

    22nd April

--- static/js/graph.js
+++ static/js/graph.js
@@ -6,13 +6,7 @@ function makeGraphs(error, attractionsJson) {
 
    //Clean attractionsJson data
    var visitorAttractionsData = attractionsJson;
-   visitorAttractionsData.forEach(function (d) {
-       d["2011_visitors"] = +d["2011_visitors"];
-       d["2012_visitors"] = +d["2012_visitors"];
-       d["2013_visitors"] = +d["2013_visitors"];
-       d["2014_visitors"] = +d["2014_visitors"];
-       d["2015_visitors"] = +d["2015_visitors"];
-   });
+
 
    //Create a Crossfilter instance
    var ndx = crossfilter(visitorAttractionsData);
@@ -36,24 +30,22 @@ function makeGraphs(error, attractionsJson) {
       var totalVisitors2013Dim = ndx.dimension(function (d) {
        return d["2013_visitors"];
    });
-   var paidforDim = ndx.dimension(function (d) {
-       return d["charge"];
-   });
     var chargeDim = ndx.dimension(function (d) {
        return d["charge_band"];
    });
 
 
    //Calculate metrics
+   var numVisitsByRegion = regionDim.group().reduceSum(function(d) {return d["2015_visitors"]});
    var numAttractionsByRegion = regionDim.group();
    var numAttractionsByCategory = categoryDim.group();
-   var numAttractionsByPaid = paidforDim.group();
    var numAttractionsByChargeBand = chargeDim.group();
-   var totalVisitorsByCru = cruDim.group().reduceSum(function (d) {
-       return d["2015_visitors"];
-   });
-   var cruGroup = cruDim.group();
+   var totalVisitorsByCru = cruDim.group().reduceSum(function (d) {return d["2015_visitors"]});
 
+   totalVisitorsByCru =(totalVisitorsByCru / 1000,000).toFixed(3) * 1000,000;
+   var grp2015 = categoryDim.group().reduceSum(function(d) {return d["2015_visitors"]});
+   var grp2014 = categoryDim.group().reduceSum(function(d) {return d["2014_visitors"]});
+   var max_visitors = grp2015.top(1)[0].value;
 
    var all = ndx.groupAll();
    var totalVisitors2015 = ndx.groupAll().reduceSum(function (d) {
@@ -68,24 +60,22 @@ function makeGraphs(error, attractionsJson) {
 
    //Charts - define the chart types objects using DC.js library.
     // We also bind the charts to the div ID’s in index.html.
-  // var timeChart = dc.barChart("#time-chart");
-   var RegionChart = dc.rowChart("#region-row-chart");
+   var RegionByAttractionsChart = dc.rowChart("#regionbyattractions-row-chart");
+   var RegionByVisitsBarChart = dc.barChart("#region-visits-row-chart");
    var CategoryChart = dc.rowChart("#category-row-chart");
    var numberAttractionsND = dc.numberDisplay("#number-attractions-nd");
    var totalVisitors2015ND = dc.numberDisplay("#total-2015visitors-nd");
    var totalVisitors2014ND = dc.numberDisplay("#total-2014visitors-nd");
    var totalVisitors2013ND = dc.numberDisplay("#total-2013visitors-nd");
-   var PaidChart = dc.pieChart("#paid-chart");
    var ChargeChart = dc.pieChart("#charges-chart");
-
+   var compositeVisits = dc.compositeChart("#compositeVisits-chart");
 
    selectField = dc.selectMenu('#menu-select')
        .dimension(cruDim)
-       .group(cruGroup);
-
+       .group(totalVisitorsByCru);
 
    numberAttractionsND
-       .formatNumber(d3.format("d"))
+      .formatNumber(d3.format("d"))
        .valueAccessor(function (d) {
            return d;
        })
@@ -100,7 +90,6 @@ function makeGraphs(error, attractionsJson) {
        .formatNumber(d3.format(".3s"));
 
    totalVisitors2014ND
-
        .valueAccessor(function (d) {
            return d;
        })
@@ -108,44 +97,69 @@ function makeGraphs(error, attractionsJson) {
        .formatNumber(d3.format(".3s"));
 
     totalVisitors2013ND
-
        .valueAccessor(function (d) {
            return d;
        })
        .group(totalVisitors2013)
 
-
- RegionChart
-       .width(800)
+    RegionByAttractionsChart
+       .width(400)
        .height(200)
        .margins({top: 10, right: 50, bottom: 30, left: 50})
        .dimension(regionDim)
        .group(numAttractionsByRegion)
        .xAxis().ticks(4);
 
+    RegionByVisitsBarChart
+       .width(400)
+       .height(200)
+       .margins({top: 10, right: 50, bottom: 30, left: 80})
+       .dimension(regionDim)
+       .group(numVisitsByRegion)
+        .xUnits(dc.units.ordinal) // Tell dc.js that we're using an ordinal x-axis
+        .x(d3.scale.ordinal())
+        .colors(d3.scale.ordinal().range(["blue","red"]))
+        .transitionDuration(2500)
+       .xAxisLabel("Region")
+       .yAxis().ticks(4);
+
+
    ChargeChart
        .height(220)
-       .radius(90)
-       .innerRadius(40)
-       .transitionDuration(1500)
+       .radius(110)
+       .innerRadius(20)
+       .transitionDuration(2500)
        .dimension(chargeDim)
        .group(numAttractionsByChargeBand);
 
    CategoryChart
        .width(300)
-       .height(250)
+       .height(350)
        .dimension(categoryDim)
        .group(numAttractionsByCategory)
        .xAxis().ticks(4);
 
-   PaidChart
-       .height(220)
-       .radius(90)
-       .innerRadius(40)
-       .transitionDuration(1500)
-       .dimension(paidforDim)
-       .group(numAttractionsByPaid);
-
+  // compositeVisits
+  //      .width(500)
+  //      .height(220)
+  //     .margins({top: 10, right: 50, bottom: 30, left: 70})
+   //     .x(d3.scale.linear().domain([0,max_visitors]))
+  //      .yAxisLabel("Categories")
+        //legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+  //      .renderHorizontalGridLines(true)
+  //      .compose([
+  //          dc.lineChart(compositeVisits)
+  //              .dimension(grp2015)
+  //              .colors('red')
+  //              .group(categoryDim, "Top Line")
+   //             .dashStyle([2,2]),
+  //          dc.lineChart(compositeVisits)
+  //              .dimension(grp2014)
+  //              .colors('blue')
+  //              .group(categoryDim, "Bottom Line")
+  //              .dashStyle([5,5])
+  //          ])
+  //      .brushOn(false);
 
    dc.renderAll();
 }
