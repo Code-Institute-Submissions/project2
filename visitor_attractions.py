a0767a704916da5fd170eb91a405bc882846fedf
Kashmir Kalirai <kashmirk67@gmail.com>
2017-05-14 19:40:34 +0100

    Connect to MongoDB

--- visitor_attractions.py
+++ visitor_attractions.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
@@ -7,7 +8,8 @@ app = Flask(__name__)
 
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
-DBS_NAME = 'visitBritainKK'
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'visitBritainKK')
 COLLECTION_NAME = 'attractions'
 
 
@@ -37,7 +39,8 @@ def visit_attractions():
 
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+    # The MONGO_URI connection is required when hosted using a remote mongo db.
+    with MongoClient(MONGO_URI) as conn:
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
